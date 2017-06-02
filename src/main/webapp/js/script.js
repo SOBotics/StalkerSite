ec113833e0709b97f8c4da1bdeb3e91433a0626f
bhargav.h <bhargav.h@endurance.com>
2017-06-02 18:35:55 +0530

    some working files

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,19 @@
+var $button = $("#submit");
+
+$button.on('click', function(){
+    var siteName = $("#site").val();
+    var hours = $("#hours").val();
+    var url = "/stalker/api/users/suspended?site="+siteName+"&hours="&hours;
+    $.getJSON(url,function (data) {
+        $output = ""
+        $i=0
+
+        for(item in data["items"]){
+            var  userlink = '<a href="'+ item['link']+'">'+ item['username']+'</a>';
+            $i+=1;
+            $output+= '<div class="row"><div class="col-md-2">'+$i+'</div><div class="col-md-10">'+ userlink+'</div></div>'
+        }
+        $output+="<p>Quota remaining: "+data['quota']+" </p>"
+        $(".output").html($output);
+    })
+})
