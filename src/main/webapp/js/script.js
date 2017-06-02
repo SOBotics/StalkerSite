47720bafdd080ca25c146d812b24c42bd79d384d
bhargav.h <bhargav.h@endurance.com>
2017-06-02 18:46:09 +0530

    ey, I had mixed up + and &

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,13 +1,12 @@
 var $button = $("#submit");
 
 $button.on('click', function(){
-    var siteName = $("#site").val();
-    var hours = $("#hours").val();
-    var url = "/stalker/api/users/suspended?site="+siteName+"&hours="&hours;
+    var siteName = $("#inputSite").val();
+    var hours = $("#inputHours").val();
+    var url = "/stalker/api/users/suspended?site="+siteName+"&hours="+hours;
     $.getJSON(url,function (data) {
         $output = ""
         $i=0
-
         for(item in data["items"]){
             var  userlink = '<a href="'+ item['link']+'">'+ item['username']+'</a>';
             $i+=1;
